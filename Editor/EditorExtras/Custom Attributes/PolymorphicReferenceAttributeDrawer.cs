2e9421c4dcd4e5621388e9628f13583f229bd8ab
MysteryGuy108 <rmaclennan67@gmail.com>
2024-11-07 17:34:36 +0000

    Added polymorphic reference and class selection generic menus to editor extras.

---
+++ Editor/EditorExtras/Custom Attributes/PolymorphicReferenceAttributeDrawer.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Runtime.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MaroonSeal.Packages.EditorExtras {
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(PolymorphicReferenceAttribute))]
+    public class PolymorphicReferenceAttributeDrawer : PropertyDrawer
+    {
+        SerializedProperty activeProperty;
+
+        public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label) {
+
+            if (_property.propertyType != SerializedPropertyType.ManagedReference) {
+                Debug.LogError("PolymorphicReference must be called on a Managed Reference type");
+                return;
+            }
+
+            PolymorphicReferenceAttribute polymorphicReference = attribute as PolymorphicReferenceAttribute;
+
+            EditorGUI.BeginProperty(_position, _label, _property);
+
+            Rect headerPosition = _position;
+            headerPosition.width -= 15.0f; headerPosition.height = 18.0f; 
+            headerPosition.x += 15.0f;
+
+            headerPosition = EditorGUI.PrefixLabel(headerPosition, _label);
+            
+
+            string title = "Select Class";
+
+            if (_property.managedReferenceValue != null) {
+                if (!_property.managedReferenceValue.GetType().IsAbstract) { title = _property.managedReferenceValue.GetType().Name; }
+            }
+           
+            if (GUI.Button(headerPosition, title)) {
+                activeProperty = _property;
+
+                ClassSelectionGenericMenu selectionMenu = new(fieldInfo.FieldType, _property.managedReferenceValue?.GetType(), ClickHandler);
+                selectionMenu.ShowAsContext();
+            }
+            
+            EditorGUI.PropertyField(_position, _property, GUIContent.none, true);
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty _property, GUIContent _label) {
+            return EditorGUI.GetPropertyHeight(_property);
+        }
+
+        private void ClickHandler(object _returnType) {
+            Type newType = (Type)_returnType;
+            activeProperty.managedReferenceValue = FormatterServices.GetUninitializedObject(newType);
+            activeProperty.serializedObject.ApplyModifiedProperties();
+            activeProperty = null;
+        }
+#endif
+    }
+}
---
+++ Editor/EditorExtras/Generic Menus/ClassSelectionGenericMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MaroonSeal.Packages.EditorExtras {
+
+#if UNITY_EDITOR
+    public class ClassSelectionGenericMenu
+    {
+        GenericMenu genericMenu;
+
+        public ClassSelectionGenericMenu(Type _baseType, Type _current, GenericMenu.MenuFunction2 _func) {
+            genericMenu = new GenericMenu();
+
+            var subclasses = _baseType.Assembly.GetTypes().Where(type => type.IsSubclassOf(_baseType));
+            foreach(var sub in subclasses) {
+                if (sub.IsAbstract) { continue; }
+                string path = GetClassMenuString(_baseType, sub);
+                bool isCurrent = _current == sub && _current != null;
+                AddMenuSelection(genericMenu, path, isCurrent, sub, _func);
+            }
+        }
+
+        public void ShowAsContext() {
+            genericMenu.ShowAsContext();
+        }
+
+        protected void AddMenuSelection(GenericMenu _menu, string _path, bool _isCurrent, object _classType, GenericMenu.MenuFunction2 _func) {
+            _menu.AddItem(new GUIContent(_path), _isCurrent, _func, _classType);
+        }
+
+        protected string GetClassMenuString(Type _baseType, Type _classType) {
+            if (!_classType.IsSubclassOf(_baseType)) { return ""; }
+            string classPath = _classType.Name;
+            var current = _classType.BaseType;
+
+            while(current != null && current != _baseType) {
+                classPath = current.Name + "/" + classPath;
+                current = current.BaseType;
+            }
+
+            return classPath;
+        }
+    }
+#endif
+}
